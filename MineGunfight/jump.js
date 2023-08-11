daf6fd370e79992f2f6625d1021c568b750c9230
SoftOSDev <117074497+SoftOSDev@users.noreply.github.com>
2023-08-11 17:25:11 +0800

    Add files via upload

---
+++ MineGunfight/jump.js
@@ -0,0 +1,9 @@
+/*!
+  *Handing Co. 2023
+  *Writting for Xihao Hua
+  *
+  *All rights reserved by Handing
+*/
+if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+  window.location.href = "./mobie"; 
+}
